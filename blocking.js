ba9848ea84df05903512ac6f8c93152945d28901
Ayush <Ayushk0004@gmail.com>
2025-07-10 22:48:42 +0530

    blocking in javascript modified

--- blocking.js
+++ blocking.js
@@ -1,14 +1,36 @@
 const fs = require("fs");
 
-console.log("Starting reading file.....");
+console.log("Start reading file...");
 
-const data = fs.readFileSync("file.txt", "utf8", (err, data) => {
-    if(err) {
-        console.error(err);
-        return;
-    }
-});
-
+const data = fs.readFileSync("file.txt", "utf8"); // Blocking
 console.log(data);
 
-console.log("Done reading file......");
+console.log("Done reading file.");
+
+/*
+Start reading file...
+<contents of file.txt>
+Done reading file.
+*/
+
+// non blocking
+const fs = require("fs");
+
+console.log("Start reading file...");
+
+fs.readFile("file.txt", "utf8", (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
+
+console.log("Done reading file.");
+
+/**
+Start reading file...
+Done reading file.
+<contents of file.txt> (after some time)
+
+ */
